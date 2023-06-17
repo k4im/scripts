9bd0a99cdb2f8e37e49aa29134cfe385db596480
k4im <contato.vitorsantos@hotmail.com>
2023-06-17 17:54:23 -0300

    Adequado script de commit

--- github-config/script-git-commit.sh
+++ github-config/script-git-commit.sh
@@ -6,6 +6,8 @@ read -p "Por favor insira a mensagem do commit: " commitMsg
 
 echo "Realizando a operaçoes git" 
 
+git config --global user.email "contato.vitorsantos@hotmail.com"
+git config --global user.name "k4im" 
 git add .; git commit -m "$commitMsg"; git push 
 
 
