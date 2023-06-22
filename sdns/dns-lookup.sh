69108e9f2b5552ee6114a7ee1b1f37373be6ae86
k4im <contato.vitorsantos@hotmail.com>
2023-06-22 14:07:35 -0300

    Atualizado sdns

--- sdns/dns-lookup.sh
+++ sdns/dns-lookup.sh
@@ -5,9 +5,16 @@ if [ -z $1 ];
 then 
 	echo "O valor não pode ser nulo"
 else 
+	echo "---------------------"
+	echo -n "MX: "
 	host -t MX $1
+	echo -n "server DNS: "
 	host -t NS $1
+	echo -n "Endereço IP: "
 	host -t A $1
+	echo -n "TXT: "
 	host -t TXT $1
+	echo -n "CNAME: "
 	host -t CNAME $1
+	echo "--------------------"
 fi
